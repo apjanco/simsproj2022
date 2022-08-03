750b87511174a3877ec9a8c5f867f71e79aabe68
apjanco <apjanco@users.noreply.github.com>
2022-08-03 21:02:12 +0000

    deploy: 01382d4be0a813364fa71acf794ca8f2398330e9

--- assets/js/lunr-store.js
+++ assets/js/lunr-store.js
@@ -1,1 +1,317 @@
-var store = [];
+var store = [{  
+"title": "Free Library of Philadelphia Lewis O 98","date": "1816","subject": "Gulshan-i Ishq","location": "Philadelphia, PA", 
+"id": "gulshan_001"
+
+},{  
+"title": "Philadelphia Museum of Art 1945-65-22(1a--255b)","date": "1743","subject": "Gulshan-i Ishq","location": "Philadelphia, PA", 
+"id": "gulshan_002"
+
+},{  
+"title": "British Library Or 11721","date": "1750","subject": "Gulshan-i Ishq","location": "London, UK", 
+"id": "gulshan_003"
+
+},{  
+"title": "British Library IO Islamic 2621","subject": "Gulshan-i Ishq","location": "London, UK", 
+"id": "gulshan_004"
+
+},{  
+"title": "British Library IO Islamic 3040","subject": "Gulshan-i Ishq","location": "London, UK", 
+"id": "gulshan_005"
+
+},{  
+"title": "British Library IO Islamic 2486","date": "1820","subject": "Gulshan-i Ishq","location": "London, UK", 
+"id": "gulshan_006"
+
+},{  
+"title": "British Library IO Islamic 1434","date": "1820","subject": "Gulshan-i Ishq","location": "London, UK", 
+"id": "gulshan_007"
+
+},{  
+"title": "British Library Or 1059","subject": "Gulshan-i Ishq","location": "London, UK", 
+"id": "gulshan_008"
+
+},{  
+"title": "Bodleian Ms Urdu e2","subject": "Gulshan-i Ishq","location": "Oxford, UK", 
+"id": "gulshan_009"
+
+},{  
+"title": "Salar Jung Museum 126","date": "1699","subject": "Gulshan-i Ishq","location": "Hyderabad, India", 
+"id": "gulshan_010"
+
+},{  
+"title": "Royal Asiatic Society 254","subject": "Gulshan-i Ishq","location": "London, UK", 
+"id": "gulshan_012"
+
+},{  
+"title": "Chhatrapata Shivaji Maharaj Vatsu Sangrahalaya M45","date": "1711","subject": "Gulshan-i Ishq","location": "Mumbai, India", 
+"id": "gulshan_013"
+
+},{  
+"title": "Aga Khan Museum AKM167","date": "1670","subject": "Gulshan-i Ishq","location": "Toronto, Canada", 
+"id": "gulshan_014"
+
+},{  
+"title": "Punjab University Library 6503","subject": "Bahar-i Danish","location": "Lahore, Pakistan", 
+"id": "bahar_001"
+
+},{  
+"title": "Punjab University Library 8959","subject": "Bahar-i Danish","location": "Lahore, Pakistan", 
+"id": "bahar_002"
+
+},{  
+"title": "Punjab University Library Pi IX 21A / 1230","subject": "Bahar-i Danish","location": "Lahore, Pakistan", 
+"id": "bahar_003"
+
+},{  
+"title": "Punjab University Library Pi IX 35 / 1444","subject": "Bahar-i Danish","location": "Lahore, Pakistan", 
+"id": "bahar_004"
+
+},{  
+"title": "Ganjbakhsh Library 15593","subject": "Bahar-i Danish","location": "Islamabad, Pakistan", 
+"id": "bahar_005"
+
+},{  
+"title": "Ganjbakhsh Library 105","subject": "Bahar-i Danish","location": "Islamabad, Pakistan", 
+"id": "bahar_006"
+
+},{  
+"title": "Ganjbakhsh Library 4465","subject": "Bahar-i Danish","location": "Islamabad, Pakistan", 
+"id": "bahar_007"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 55","date": "1712","subject": "Bahar-i Danish","location": "Hyderabad, India", 
+"id": "bahar_008"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 56","date": "1774","subject": "Bahar-i Danish","location": "Hyderabad, India", 
+"id": "bahar_009"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 54","date": "1789","subject": "Bahar-i Danish","location": "Hyderabad, India", 
+"id": "bahar_010"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 59","date": "1836","subject": "Bahar-i Danish","location": "Hyderabad, India", 
+"id": "bahar_011"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 57","date": "1822","subject": "Bahar-i Danish","location": "Hyderabad, India", 
+"id": "bahar_012"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 53","date": "1800","subject": "Bahar-i Danish","location": "Hyderabad, India", 
+"id": "bahar_013"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 52","date": "1902","subject": "Bahar-i Danish","location": "Hyderabad, India", 
+"id": "bahar_014"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 58","date": "1902","subject": "Bahar-i Danish","location": "Hyderabad, India", 
+"id": "bahar_015"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 60","date": "1902","subject": "Bahar-i Danish","location": "Hyderabad, India", 
+"id": "bahar_016"
+
+},{  
+"title": "Khalsa College, Amritsar S. H. R. 2121","subject": "Bahar-i Danish","location": "Amritsar, India", 
+"id": "bahar_017"
+
+},{  
+"title": "Princeton Islamic Manuscripts New Series no. 1771","date": "1700","subject": "Bahar-i Danish","location": "Princeton, NJ", 
+"id": "bahar_018"
+
+},{  
+"title": "Princeton Islamic Manuscripts, New Series no. 1232","date": "1700","subject": "Bahar-i Danish","location": "Princeton, NJ", 
+"id": "bahar_019"
+
+},{  
+"title": "National Archives of Pakistan Adab 82","subject": "Bahar-i Danish","location": "Karachi, Pakistan", 
+"id": "bahar_020"
+
+},{  
+"title": "Edinburgh University Library call no 1","date": "1822","subject": "Bahar-i Danish","location": "Edinburgh, UK", 
+"id": "bahar_021"
+
+},{  
+"title": "Edinburgh University Library call no 2","date": "1822","subject": "Bahar-i Danish","location": "Edinburgh, UK", 
+"id": "bahar_022"
+
+},{  
+"title": "Hungarian Academy of Science Persza Qu. 38","subject": "Bahar-i Danish","location": "Budapest, Hungary", 
+"id": "bahar_023"
+
+},{  
+"title": "Hungarian Academy of Science Persza O. 078","subject": "Bahar-i Danish","location": "Budapest, Hungary", 
+"id": "bahar_025"
+
+},{  
+"title": "Hungarian Academy of Science Persza Qu. 39","subject": "Bahar-i Danish","location": "Budapest, Hungary", 
+"id": "bahar_026"
+
+},{  
+"title": "McGill University Blacker-Wood Library W1","date": "1830","subject": "Bahar-i Danish","location": "Montreal, Canada", 
+"id": "bahar_027"
+
+},{  
+"title": "McGill University Blacker-Wood Library W53","date": "1821","subject": "Bahar-i Danish","location": "Montreal, Canada", 
+"id": "bahar_028"
+
+},{  
+"title": "Harvard Houghton Library MS Persian 24","date": "1732","subject": "Tutinama","location": "Cambridge, MA", 
+"id": "tuti_001"
+
+},{  
+"title": "John Rylands Library University of Manchester Persian MS 215","subject": "Tutinama","location": "Manchester, UK", 
+"id": "tuti_002"
+
+},{  
+"title": "John Rylands Library University of Manchester Persian MS 257","subject": "Tutinama","location": "Manchester, UK", 
+"id": "tuti_003"
+
+},{  
+"title": "John Rylands Library University of Manchester Persian MS 519","subject": "Tutinama","location": "Manchester, UK", 
+"id": "tuti_004"
+
+},{  
+"title": "Eton College Library Pote 274","subject": "Tutinama","location": "Windsor, UK", 
+"id": "tuti_005"
+
+},{  
+"title": "Eton College Library Pote 371","subject": "Tutinama","location": "Windsor, UK", 
+"id": "tuti_006"
+
+},{  
+"title": "Bodleian Library Ms Bodl. Or. 760","subject": "Tutinama","location": "Oxford, UK", 
+"id": "tuti_007"
+
+},{  
+"title": "Bodleian Library MS. Fraser 102","subject": "Tutinama","location": "Oxford, UK", 
+"id": "tuti_008"
+
+},{  
+"title": "Bodleian Library MS. Ind. Inst. Pers. 68","subject": "Tutinama","location": "Oxford, UK", 
+"id": "tuti_009"
+
+},{  
+"title": "Bodleian Library MS. Ouseley 50","subject": "Tutinama","location": "Oxford, UK", 
+"id": "tuti_010"
+
+},{  
+"title": "Bodleian Library MS. Ouseley 85, 86","subject": "Tutinama","location": "Oxford, UK", 
+"id": "tuti_011"
+
+},{  
+"title": "Bodleian Library MS. Ouseley 262","subject": "Tutinama","location": "Oxford, UK", 
+"id": "tuti_012"
+
+},{  
+"title": "Bodleian Library MS. S. Digby Or. 116","subject": "Tutinama","location": "Oxford, UK", 
+"id": "tuti_013"
+
+},{  
+"title": "Bodleian Library MS. S. Digby Or. 162","subject": "Tutinama","location": "Oxford, UK", 
+"id": "tuti_014"
+
+},{  
+"title": "Bodleian Library MS. S. Digby Or. 163","subject": "Tutinama","location": "Oxford, UK", 
+"id": "tuti_015"
+
+},{  
+"title": "SOAS University of London Library ms. 46717","subject": "Tutinama","location": "London, UK", 
+"id": "tuti_016"
+
+},{  
+"title": "SOAS University of London Library ms. 44561","subject": "Tutinama","location": "London, UK", 
+"id": "tuti_017"
+
+},{  
+"title": "SOAS University of London Library ms. 46521","subject": "Tutinama","location": "London, UK", 
+"id": "tuti_018"
+
+},{  
+"title": "University of St Andrews Library ms41(o)","subject": "Tutinama","location": "St Andrews, UK", 
+"id": "tuti_019"
+
+},{  
+"title": "Cambridge University Library Ff. 2. 21","subject": "Tutinama","location": "Cambridge, UK", 
+"id": "tuti_020"
+
+},{  
+"title": "Cambridge University Library Add. 187","subject": "Tutinama","location": "Cambridge, UK", 
+"id": "tuti_021"
+
+},{  
+"title": "Cambridge University Library D. 21","subject": "Tutinama","location": "Cambridge, UK", 
+"id": "tuti_022"
+
+},{  
+"title": "Columbia University Rare Book and Manuscript Library X892.8T88 O","date": "1850","subject": "Tutinama","location": "New York, NY", 
+"id": "tuti_023"
+
+},{  
+"title": "Princeton Islamic Manuscripts, New Series no. 1804","date": "1723","subject": "Tutinama","location": "Princeton, NJ", 
+"id": "tuti_024"
+
+},{  
+"title": "Edinburgh University Library","subject": "Tutinama","location": "Edinburgh, UK", 
+"id": "tuti_025"
+
+},{  
+"title": "Munich","subject": "Tutinama","location": "Munich, Germany", 
+"id": "tuti_026"
+
+},{  
+"title": "Leipzig","subject": "Tutinama","location": "Leipzig, Germany", 
+"id": "tuti_027"
+
+},{  
+"title": "Berlin","subject": "Tutinama","location": "Berlin, Germany", 
+"id": "tuti_028"
+
+},{  
+"title": "Hungarian Academy of Sciences Persza O. 060","subject": "Tutinama","location": "Budapest, Hungary", 
+"id": "tuti_029"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 98","date": "1722","subject": "Tutinama","location": "Hyderabad, India", 
+"id": "tuti_030"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 128","date": "1843","subject": "Tutinama","location": "Hyderabad, India", 
+"id": "tuti_031"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 189","date": "1853","subject": "Tutinama","location": "Hyderabad, India", 
+"id": "tuti_032"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 97","date": "1822","subject": "Tutinama","location": "Hyderabad, India", 
+"id": "tuti_033"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 191","date": "1851","subject": "Tutinama","location": "Hyderabad, India", 
+"id": "tuti_034"
+
+},{  
+"title": "Salar Jung Museum and Library A.N. 190","date": "1831","subject": "Tutinama","location": "Hyderabad, India", 
+"id": "tuti_035"
+
+},{  
+"title": "McGill University Rare Books & Special Collections Division 13","date": "1802","subject": "Tutinama","location": "Montreal, Canada", 
+"id": "tuti_036"
+
+},{  
+"title": "McGill University Blacker-Wood Library 18","date": "1809","subject": "Tutinama","location": "Montreal, Canada", 
+"id": "tuti_037"
+
+},{  
+"title": "McGill University Blacker-Wood Library 17","date": "1900","subject": "Tutinama","location": "Montreal, Canada", 
+"id": "tuti_038"
+
+},{  
+"title": "Columbia University Rare Book and Manuscript Library X892.8 Ad9","date": "1800","subject": "Haft Sair-i Hatim","location": "New York, NY", 
+"id": "hatim_001"
+
+}];
